7486bc58337136dcc93b924e5af9d7aaac4538d1
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-01-04 17:30:04 +0700

    page43

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form : web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
@@ -11,7 +22,10 @@ async fn health_check() -> HttpResponse {
 // without having to use any proc-macro incantation
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
+        App::new()
+        .app_data(web::FormConfig::default())
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscription", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,26 @@
 //! tests/health_check.rs
 use std::net::TcpListener;
+
+// No .await call, therefore no need for 'spawn_app' to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can jsut panic and crash
+// all the things
+fn spawn_app() -> String {
+    let listener =
+        TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    //wwwwwwwww
+    // New dev dependency - let's add tokio to the party with
+    // 'cargo add tokio --dev --vers 1'
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 // 'actix_rt::test' is the testing equivalent of 'actix_web::main'.
 // It also spares you from having to specify the '#[test]' attribute/
 //
@@ -31,22 +52,53 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// No .await call, therefore no need for 'spawn_app' to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can jsut panic and crash
-// all the things
-fn spawn_app() -> String {
-    let listener =
-        TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    //wwwwwwwww
-    // New dev dependency - let's add tokio to the party with
-    // 'cargo add tokio --dev --vers 1'
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+#[actix_web::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
+    let response = client
+        .post(&format!("{}/subscription", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_web::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscription", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "the api did not fail with 400 bad request when the payload was {}",
+            error_message
+        );
+    }
 }
