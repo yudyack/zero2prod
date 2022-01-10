19a8fe27a96ad206e18c251acf7bb601a322e8e8
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-01-10 14:36:23 +0700

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,22 @@ use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // We removed the `env_logger` line we had before!
-    // env_logger::Builder::from_env(Env::default().default_filter_or("info"))
-    //     .init();
-
-    // Redirect all `log`'s events to our subscriber
     LogTracer::init().expect("Failed to set logger");
 
-    // We are falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
 
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
-        // output the formatted to stdout
         std::io::stdout,
     );
 
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
     set_global_default(subscriber).expect("Failed to set subscriber");
 
-    // panic if we can't read configuration file
     let configuration =
         get_configuration().expect("failed to read configuration");
 
@@ -50,10 +37,5 @@ async fn main() -> std::io::Result<()> {
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind port");
-    // We retrieve the port assigned to us by the OS
-    // let port = listener.local_addr().unwrap().port();
-
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
     run(listener, connection_pool)?.await
 }
