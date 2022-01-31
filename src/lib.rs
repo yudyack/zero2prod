8835bee3872e16270b7981d25ea69aedc095a562
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-01-31 15:52:39 +0700

    add parse and inner ref, test still failed

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! src/lib.rs
 // make public to other binaries (main, test)
 pub mod configuration;
+pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,12 @@ use actix_web::HttpResponse;
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+// An extension trait to provide the `graphemes` method
+// on `String` and `&str`
+use unicode_segmentation::UnicodeSegmentation;
+
+use crate::domain::NewSubscriber;
+use crate::domain::SubscriberName;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -24,19 +30,51 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    match insert_subscriber(&form, &connection_pool).await {
+    // if !is_valid_name(&form.name) {
+    //     return HttpResponse::BadRequest().finish();
+    // }
+    // let form1: FormData = form.0;
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+    };
+
+    match insert_subscriber(&connection_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
+/// Returns `true` if the input satisfies all our validation constraints
+/// on subscriber names, `false` otherwise.
+pub fn is_valid_name(s: &str) -> bool {
+    // `.trim()` returns a view over the input `s` without trailing
+    // whitespace-like characters.
+    // `.is_empty` checks if the view contains any character.
+    let is_empty_or_whitespace = s.trim().is_empty();
+    // A grapheme is defined by the Unicode standard as a "user-perceived"
+    // character: `å` is a single grapheme, but it is composed of two characters
+    // (`a` and ``).
+    //
+    // `graphemes` returns an iterator over the graphemes in the input `s`.
+    // `true` specifies that we want to use the extended grapheme definition set,
+    // the recommended one.
+    let is_too_long = s.graphemes(true).count() > 256;
+    // Iterate over all characters in the input `s` to check if any of them matches
+    // one of the characters in the forbidden array.
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters =
+        s.chars().any(|g| forbidden_characters.contains(&g));
+    // Return `false` if any of our conditions have been violated
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
+}
 
 #[tracing::instrument(
     name = "Saving new subscriber detail in the database",
-    skip(form, connection_pool)
+    skip(new_subscriber, connection_pool)
 )]
 pub async fn insert_subscriber(
-    form: &FormData,
     connection_pool: &PgPool,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     let _a = sqlx::query!(
         r#"
@@ -44,8 +82,8 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.inner_ref(),
         Utc::now()
     )
     .execute(connection_pool)
