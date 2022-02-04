98a5054e5844478670b552ab8dd471abb3f0056e
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-02-04 10:21:13 +0700

    page 187 add error for status

--- src/email_client.rs
+++ src/email_client.rs
@@ -52,7 +52,8 @@ impl EmailClient {
             )
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         Ok(())
     }
 }
@@ -75,8 +76,9 @@ mod tests {
     use fake::Faker;
     use secrecy::Secret;
     use serde_json::Error;
-    use wiremock::matchers::{header, header_exists, method, path};
+    use wiremock::matchers::{header, header_exists, method, path, any};
     use wiremock::{Mock, MockServer, ResponseTemplate};
+    use claim::assert_err;
 
     use crate::domain::SubscriberEmail;
 
@@ -136,5 +138,68 @@ mod tests {
 
         // Assert
         assert!(result.is_ok());
+    }
+
+
+    #[tokio::test]
+    async fn send_email_succeeds_if_the_serer_returns_200() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = super::EmailClient::new(
+            mock_server.uri(),
+            sender,
+            Secret::new(Faker.fake()),
+        );
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email =
+            SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..2).fake();
+
+        // Act
+        let result = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = super::EmailClient::new(
+            mock_server.uri(),
+            sender,
+            Secret::new(Faker.fake()),
+        );
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email =
+            SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..2).fake();
+
+        // Act
+        let result = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert_err!(result);
     }
 }
