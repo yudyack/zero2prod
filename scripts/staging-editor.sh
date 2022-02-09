8bdb5b3759e7d25d04810aedf8bed433e71a1f74
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-02-09 17:04:49 +0700

    edit staging-editor script

--- scripts/staging-editor.sh
+++ scripts/staging-editor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-git add .
-cargo fmt --all --check
-git add .
-cargo fix --allow-dirty
+git add . 
+cargo fmt --all 
+git add . 
+cargo fix --allow-staged
 git add .
 cargo sqlx prepare -- --lib
 git add .
