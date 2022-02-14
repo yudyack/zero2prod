08c281b0c2e9fd821d822a2d4c6ad3645de4e414
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-02-14 17:01:38 +0700

    print exception by implement debug and display (Page 254)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -189,7 +189,6 @@ pub async fn store_token(
     Ok(())
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl ResponseError for StoreTokenError {}
@@ -202,4 +201,29 @@ impl std::fmt::Display for StoreTokenError {
         trying to store a subscription token."
         )
     }
+}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
 }
