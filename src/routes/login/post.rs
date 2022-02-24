370912849d21734fc63b0840e16697ba36f13fdb
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-02-24 15:15:51 +0700

    compiled (Page338)

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,8 @@
 use actix_web::http::header::LOCATION;
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, ResponseError};
-use secrecy::Secret;
+use hmac::{Hmac, Mac};
+use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 use sqlx::PgPool;
 
@@ -22,7 +23,9 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-) -> Result<HttpResponse, LoginError> {
+    secret: web::Data<Secret<String>>,
+    // No longer returning a `Result<HttpResponse, LoginError>`!
+) -> HttpResponse {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
@@ -31,21 +34,45 @@ pub async fn login(
     tracing::Span::current()
         .record("username", &tracing::field::display(&credentials.username));
 
-    let user_id = validate_credentials(credentials, &pool).await.map_err(
-        |e| match e {
-            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
-            AuthError::UnexpectedError(_) => {
-                LoginError::UnexpectedError(e.into())
-            }
-        },
-    )?;
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current()
+                .record("user_id", &tracing::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(e) => {
+            let e = match e {
+                AuthError::InvalidCredentials(_) => {
+                    LoginError::AuthError(e.into())
+                }
+                AuthError::UnexpectedError(_) => {
+                    LoginError::UnexpectedError(e.into())
+                }
+            };
 
-    tracing::Span::current()
-        .record("user_id", &tracing::field::display(&user_id));
-
-    Ok(HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish())
+            let query_string =
+                format!("error={}", urlencoding::Encoded::new(e.to_string()));
+            // We need the secret here
+            let hmac_tag = {
+                let mut mac = Hmac::<sha2::Sha256>::new_from_slice(
+                    secret.expose_secret().as_bytes(),
+                )
+                .unwrap();
+                mac.update(query_string.as_bytes());
+                mac.finalize().into_bytes()
+            };
+            // Appending the hexadecimal representation of the HMAC tag to the
+            // query string as an additional query parameter.
+            HttpResponse::SeeOther()
+                .insert_header((
+                    LOCATION,
+                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
+                ))
+                .finish()
+        }
+    }
 }
 
 #[derive(thiserror::Error)]
@@ -59,25 +86,5 @@ pub enum LoginError {
 impl std::fmt::Debug for LoginError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
-    }
-}
-
-impl ResponseError for LoginError {
-    fn error_response(&self) -> HttpResponse {
-        let encoded_error = urlencoding::Encoded::new(self.to_string());
-        HttpResponse::build(self.status_code())
-            .insert_header((
-                LOCATION,
-                format!("/login?error={}", encoded_error),
-            ))
-            .finish()
-    }
-
-    fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            // Return a 401 for auth errors
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
     }
 }
