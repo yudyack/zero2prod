d7c215898da43807325a7afbecd3e848af15d357
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-03-08 11:31:22 +0700

    format

--- scripts/staging-editor.sh
+++ scripts/staging-editor.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 git add . 
-cargo fmt --all 
-git add . 
 cargo fix --allow-staged
 git add .
 cargo clippy
 git add .
 cargo sqlx prepare -- --lib
 git add .
+cargo fmt --all 
+git add . 
