a96c2e727b3884d506a91a3d2a2151c84f90cf11
Yudhya P W <yudhya.wicaksono@outlook.com>
2022-03-17 15:11:08 +0700

    change port db back to 5432

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -22,7 +22,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newslettery}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5433}"
+DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
 
